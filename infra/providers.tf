7b951bc4b52c2b859855618c7a3073c23753179a
abdikarimmhassan <abdikarimmh98@gmail.com>
2026-01-17 07:06:14 +0000

    add helm and aws providers

---
+++ infra/providers.tf
@@ -0,0 +1,37 @@
+terraform {
+  backend "s3" {
+    bucket = "abdikarim-eks-terraform-state"
+    key    = "eks-prod-terraform.tfstate"
+    region = "eu-north-1"
+  }
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name,
+        "--region",
+        
+      ]
+    }
+  }
+}
