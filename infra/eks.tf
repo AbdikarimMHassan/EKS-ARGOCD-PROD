6a323781ad04e3fbd333a63672aefa7388031d9d
abdikarimmhassan <abdikarimmh98@gmail.com>
2026-01-19 14:10:09 +0000

    add eks cluster

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,39 +1,43 @@
 module "eks" {
-  source                         = "terraform-aws-modules/eks/aws"
-  version                        = "20.2.0"
-  cluster_name                   = local.name
-  cluster_version                = "1.27"
-  cluster_endpoint_public_access = true #accessable from my ip only
-  cluster_endpoint_public_access_cidrs  = [var.my_ip]
-  enable_irsa                    = true
-  subnets                        = module.vpc.private_subnets
-  control_plane_subnet_ids       = module.vpc.public_subnets
-  vpc_id                         = module.vpc.vpc_id
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
 
-  eks_managed_node_group_defaults = {
-    disk_size      = 50
-    instance_types = ["t3a.large", "t3.large"]
-  }
+  cluster_name    = local.name
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_public_access_cidrs = [var.my_ip]
+
+  enable_irsa = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  control_plane_subnet_ids = module.vpc.public_subnets
 
   eks_managed_node_groups = {
-    defaults = {}
+    default = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 1
+      instance_types = ["t3a.large", "t3.large"]
+      disk_size      = 50
+    }
   }
 
-  tags = local.tags
-
-}
-
-
-access_entries = {
-  admin = {
-    principal_arn = "arn:aws:iam::779846800049:user/akarim"
-    policy_associations = {
-      admin = {
-        policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-        access_scope = {
-          type = "cluster"
+  access_entries = {
+    admin = {
+      principal_arn = "arn:aws:iam::779846800049:user/akarim"
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
         }
       }
     }
   }
+
+  tags = local.tags
 }
